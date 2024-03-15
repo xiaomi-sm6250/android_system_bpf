62ca935e34a75642e965821f5cdc88e5d49a8747
Maciej Żenczykowski <maze@google.com>
2024-03-15 19:03:13 +0000

    bpfloader: more 4.9-T support
    (older kernels don't support naming maps/programs)
    
    Test: TreeHugger
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: If59b8947021394e1ce0a91326fee4272a5bddd38

--- loader/Loader.cpp
+++ loader/Loader.cpp
@@ -620,7 +620,8 @@ static int createMaps(const char* elfPath, ifstream& elfFile, vector<unique_fd>&
               .max_entries = max_entries,
               .map_flags = md[i].map_flags,
             };
-            strlcpy(req.map_name, mapNames[i].c_str(), sizeof(req.map_name));
+            if (isAtLeastKernelVersion(4, 14, 0))
+                strlcpy(req.map_name, mapNames[i].c_str(), sizeof(req.map_name));
             fd.reset(bpf(BPF_MAP_CREATE, req));
             saved_errno = errno;
             ALOGV("bpf_create_map name %s, ret: %d", mapNames[i].c_str(), fd.get());
@@ -779,7 +780,8 @@ static int loadCodeSections(const char* elfPath, vector<codeSection>& cs, const 
               .log_buf = ptr_to_u64(log_buf.data()),
               .log_size = static_cast<__u32>(log_buf.size()),
             };
-            strlcpy(req.prog_name, cs[i].name.c_str(), sizeof(req.prog_name));
+            if (isAtLeastKernelVersion(4, 14, 0))
+                strlcpy(req.prog_name, cs[i].name.c_str(), sizeof(req.prog_name));
             fd.reset(bpf(BPF_PROG_LOAD, req));
 
             if (!fd.ok()) {
